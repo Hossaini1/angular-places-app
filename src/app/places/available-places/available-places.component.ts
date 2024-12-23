886ce9c29d0aa74a33a6e4f1792554e87f1c5ef3
ali <1989hossaini1@gmail.com>
2024-12-23 11:18:43 +0100

    added backend and frontend with httpclint configuration

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  // alternative
+  // constructor(private httpClient:HttpClient){}
+
+  ngOnInit():void{
+
+  }
+
+
+}
