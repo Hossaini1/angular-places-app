99f6967d5e6bdddb4c617846a0552fdb88d010ad
ali <1989hossaini1@gmail.com>
2024-12-25 15:45:28 +0100

    loading handeling with signal

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -5,15 +5,17 @@ import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-available-places',
   standalone: true,
   templateUrl: './available-places.component.html',
   styleUrl: './available-places.component.css',
-  imports: [PlacesComponent, PlacesContainerComponent],
+  imports: [PlacesComponent, PlacesContainerComponent,CommonModule],
 })
 export class AvailablePlacesComponent implements OnInit {
+  isFetching = signal(false);
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
   // alternative
@@ -22,13 +24,19 @@ export class AvailablePlacesComponent implements OnInit {
   private destroyRef = inject(DestroyRef)
 
   ngOnInit(): void {
-    const subscription = this.httpClient.get<{places:Place[]}>('http://localhost:4000/places'
+    this.isFetching.set(true);
+
+    const subscription = this.httpClient.get<{places:Place[]}>('http://localhost:3000/places'
     ).pipe(map((resDataObj)=>resDataObj.places)).subscribe({
       next: (resArray) => {
         this.places.set(resArray)
         console.log(resArray);
         
+      },
+      complete:()=>{
+        this.isFetching.set(false)
       }
+
     });
     this.destroyRef.onDestroy(() => subscription.unsubscribe())
 
