fab441d3b63ce5e511278d091c4fd2862cffb8c5
ali <1989hossaini1@gmail.com>
2024-12-27 10:42:09 +0100

    emit event und send data zu backend und favorite places anzeigen wurde geuebt

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -48,5 +48,14 @@ export class AvailablePlacesComponent implements OnInit {
     });
     this.destroyRef.onDestroy(() => subscription.unsubscribe())
 
+  };
+
+  onSelectPlace(selectedPlace:Place){
+    this.httpClient.put('http://localhost:3000/user-places',{
+      placeId:selectedPlace.id
+    }).subscribe({
+      next:(resData)=>console.log(resData)
+      
+    })
   }
 }
--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,26 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  favoritePlaces = signal<Place[] | undefined>(undefined);
+  isLoading = signal(false);
+  errorMsg = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.isLoading.set(true);
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resDataObj) => resDataObj.places), catchError((err) => {
+          console.log(err);
+          return throwError(() => new Error('Something went wrong fetching favorite places! please try again.'))
+        })
+      ).subscribe({
+        next: (resData) => this.favoritePlaces.set(resData),
+        complete: () => this.isLoading.set(false),
+        error: (err) => this.errorMsg.set(err.message)
+      })
+    this.destroyRef.onDestroy(() => subscription.unsubscribe())
+  }
 }
