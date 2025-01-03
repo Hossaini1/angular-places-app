5aed1a88b8ba3a08694f5a6941bdc5185b16c09c
ali <1989hossaini1@gmail.com>
2025-01-03 09:39:09 +0100

    interceptor und header modifizierung in angular 17

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+function logginInspector(request:HttpRequest<unknown>,next:HttpHandlerFn):Observable<HttpEvent<unknown>>{
+
+    // Hier nur infos sehen
+    // console.log('Outputing Request');
+    // console.log(request);
+    // return next(request);
+
+    // Hier infos sehen und etwas hinzufügen in header!
+    // mit clone und new request konnen wir ZB. header ändern
+
+    // const modifierteRequest = request.clone({
+    //     headers:request.headers.set('X-DEBUG','TESTING')
+    // })
+    console.log('Outputing Request');
+    console.log(request);
+    // return next(modifierteRequest);
+    return next(request);
+}
 
 bootstrapApplication(AppComponent,{
-    providers:[provideHttpClient()]
+    providers:[provideHttpClient(withInterceptors([logginInspector]))]
 }).catch((err) => console.error(err));
