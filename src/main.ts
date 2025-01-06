4c50f1ecb795bdc8d829777073791bd3dec01e5b
ali <1989hossaini1@gmail.com>
2025-01-06 08:27:00 +0100

    resoponse handeling mit interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpEvent, HttpHandler, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpEventType, HttpHandler, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { Observable, pipe, tap } from 'rxjs';
 
-function logginInspector(request:HttpRequest<unknown>,next:HttpHandlerFn):Observable<HttpEvent<unknown>>{
+function logginInspector(request: HttpRequest<unknown>, next: HttpHandlerFn):Observable<HttpEvent<unknown>> {
 
     // Hier nur infos sehen
     // console.log('Outputing Request');
@@ -20,9 +20,21 @@ function logginInspector(request:HttpRequest<unknown>,next:HttpHandlerFn):Observ
     console.log('Outputing Request');
     console.log(request);
     // return next(modifierteRequest);
-    return next(request);
+    // hier mit pipe koennen wie response behandln
+    return next(request). pipe(
+        tap({
+            next: (event) => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('Incoming Response');
+                    console.log(event.status);
+                    console.log(event.body);
+
+                }
+            }
+        })
+    );
 }
 
-bootstrapApplication(AppComponent,{
-    providers:[provideHttpClient(withInterceptors([logginInspector]))]
+bootstrapApplication(AppComponent, {
+    providers: [provideHttpClient(withInterceptors([logginInspector]))]
 }).catch((err) => console.error(err));
